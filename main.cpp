ec28b46a726cc6681f8c3131d050e8d716ee1259
Crymsius <romain.m.mars@gmail.com>
2017-12-04 12:47:50 +0100

    Adds mac/windows cases

--- main.cpp
+++ main.cpp
@@ -6,7 +6,7 @@ int main()
 	Boxd box;
 	box.min = Math::Vec2d(0, 0);
 	box.max = Math::Vec2d(1000, 1000);
-	Scalarfield field("heightfield.png", box, 10, 12);
+	Scalarfield field("heightfield.png", box, 10, 11);
 
 	field.ExportToObj("field.obj", 300, 300);
 
