8dd9c84ef048dc9dd0202648a6906d4fe77d1f31
Crymsius <romain.m.mars@gmail.com>
2017-12-05 10:38:05 +0100

    Adds analyticHeightField and Simplex Noise

---
+++ AnalyticHeightField.hpp
@@ -0,0 +1,17 @@
+/**
+* \file Layersfield.hpp
+* \brief todo
+* \authors Crymsius, Elekhyr, Plopounet
+* \version 1.0
+* \date 02/12/17
+* \copyright Copyright (c) 2017 Romain Maurizot, Thomas Margier, Loïs Paulin
+*  This file is licensed under the MIT License, see https://opensource.org/licenses/MIT
+*/
+#pragma once
+#include "Box.hpp"
+
+class AnalyticHeightField
+	: Boxd
+{
+
+}
