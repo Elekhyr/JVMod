c0756f9eaf1e60142a4c9d2f0ab99aedf7b13dba
Crymsius <romain.m.mars@gmail.com>
2018-01-15 16:33:37 +0100

    changement slope dans le findneighboursflow

--- Field.hpp
+++ Field.hpp
@@ -65,7 +65,7 @@ protected:
 	virtual Math::Vec2d Slope(unsigned i, unsigned j) const;
 	virtual double DrainCellArea(unsigned i, unsigned j) const;
 	virtual void FindNeighboursFlow(unsigned i, unsigned j, std::vector<Math::Vec2u> NeighboursCoords, 
-		std::vector<Math::Vec2d> NeighboursSlopes, std::vector<double> NeighboursDifHeight) const;
+		std::vector<double> NeighboursSlopes, std::vector<double> NeighboursDifHeight) const;
 	virtual double Wetness(unsigned i, unsigned j) const;
 	virtual double StreamPower(unsigned i, unsigned j) const;
 	virtual double Light(unsigned i, unsigned j) const;
